7de9ba0112988411e6b8eb2979eb4275cdee7e67
Yi-Jet <ichieh@foxmail.com>
2025-06-28 16:40:10 +0800

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -1,21 +1,28 @@
-#!/bin/bash
-# 此脚本计算给定本金、年利率和时间（以年为单位）的简单利息。
-# 不要在生产环境中使用。仅供示例。
-# 作者：Upkar Lidder (IBM)
-# 其他作者：
-# <你的 GitHub 用户名>
-# 输入：
-# p，本金
-# t，时间（以年为单位）
-# r，年利率
-# 输出：
-# 简单利息 = p*t*r
-echo "请输入本金："
-read p
-echo "请输入年利率："
-read r
-echo "请输入时间（以年为单位）："
-read t
-s=`expr $p \* $t \* $r / 100`
-echo "简单利息是："
-echo $s
+   #!/bin/bash
+   # This script calculates simple interest given principal,
+   # annual rate of interest and time period in years.
+
+   # Do not use this in production. Sample purpose only.
+
+   # Author: Upkar Lidder (IBM)
+   # Additional Authors:
+   # <your GitHub username>
+
+   # Input:
+   # p, principal amount
+   # t, time period in years
+   # r, annual rate of interest
+
+   # Output:
+   # simple interest = p*t*r
+
+   echo "Enter the principal:"
+   read p
+   echo "Enter rate of interest per year:"
+   read r
+   echo "Enter time period in years:"
+   read t
+
+   s=`expr $p \* $t \* $r / 100`
+   echo "The simple interest is: "
+   echo $s
